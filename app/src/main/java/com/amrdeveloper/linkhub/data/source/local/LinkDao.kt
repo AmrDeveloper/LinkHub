5760f52ee3d4d91e42e6fd8e7bf7ef21996e1344
AmrDeveloper <amr96@programmer.net>
2021-06-21 16:39:33 +0200

    Fix link database sorting

--- app/src/main/java/com/amrdeveloper/linkhub/data/source/local/LinkDao.kt
+++ app/src/main/java/com/amrdeveloper/linkhub/data/source/local/LinkDao.kt
@@ -10,16 +10,16 @@ interface LinkDao : BaseDao<Link> {
     @Query("SELECT * FROM link")
     suspend fun getLinkList(): List<Link>
 
-    @Query("SELECT * FROM link ORDER BY pinned DESC, click_count ASC")
+    @Query("SELECT * FROM link ORDER BY pinned DESC, click_count DESC")
     suspend fun getSortedLinkList(): List<Link>
 
-    @Query("SELECT * FROM link WHERE folder_id = :id ORDER BY pinned  DESC, click_count ASC")
+    @Query("SELECT * FROM link WHERE folder_id = :id ORDER BY pinned  DESC, click_count DESC")
     suspend fun getSortedLinkListByFolderId(id: Int): List<Link>
 
-    @Query("SELECT * FROM link WHERE title LIKE '%' || :keyword || '%' ORDER BY pinned  DESC, click_count ASC")
+    @Query("SELECT * FROM link WHERE title LIKE '%' || :keyword || '%' ORDER BY pinned  DESC, click_count DESC")
     suspend fun getSortedLinkListByKeyword(keyword : String) : List<Link>
 
-    @Query("SELECT * FROM link WHERE folder_id = :id AND title LIKE '%' || :keyword || '%' ORDER BY pinned  DESC, click_count ASC")
+    @Query("SELECT * FROM link WHERE folder_id = :id AND title LIKE '%' || :keyword || '%' ORDER BY pinned  DESC, click_count DESC")
     suspend fun getSortedLinkListByKeywordByFolderId(id: Int, keyword : String) : List<Link>
 
     @Query("UPDATE link SET click_count = :count WHERE id = :linkId")
