fdfff0deeba1f1ddf87ea59d39faf7b790546284
AmrDeveloper <amr96@programmer.net>
2021-07-04 17:40:37 +0200

    Write testing for link dao

---
+++ app/src/androidTest/java/com/amrdeveloper/linkhub/data/source/local/LinkDaoTest.kt
@@ -0,0 +1,93 @@
+package com.amrdeveloper.linkhub.data.source.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.amrdeveloper.linkhub.data.Link
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+class LinkDaoTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: LinkRoomDatabase
+    private lateinit var linkDao : LinkDao
+
+    @Before
+    fun setupDatabase() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            LinkRoomDatabase::class.java
+        ).allowMainThreadQueries().build()
+        linkDao = database.linkDao()
+    }
+
+    @Test
+    fun insert_uniqueLink() = runBlockingTest {
+        val link = Link("title", "subtitle", "url")
+        val insertResult = linkDao.insert(link)
+        assertThat(insertResult).isGreaterThan(-1)
+    }
+
+    @Test
+    fun insert_unUniqueLink() = runBlockingTest {
+        val link = Link("title", "subtitle", "url")
+        linkDao.insert(link)
+        val insertResult = linkDao.insert(link)
+        assertThat(insertResult).isEqualTo(-1)
+    }
+
+    @Test
+    fun read_sortedLinkListByClickCount() = runBlockingTest {
+        val link = Link("title", "subtitle", "url", clickedCount = 5)
+        linkDao.insert(link)
+
+        val link2 = Link("title2", "subtitle2", "url2", clickedCount = 10)
+        linkDao.insert(link2)
+
+        val sortedList = linkDao.getSortedLinkList()
+        assertThat(sortedList.first().title).isEqualTo("title2")
+    }
+
+    @Test
+    fun read_sortedLinkListByPinned() = runBlockingTest {
+        val link = Link("title", "subtitle", "url")
+        linkDao.insert(link)
+
+        val link2 = Link("title2", "subtitle2", "url2", isPinned = true)
+        linkDao.insert(link2)
+
+        val sortedList = linkDao.getSortedLinkList()
+        assertThat(sortedList.first().title).isEqualTo("title2")
+    }
+
+    @Test
+    fun read_sortedLinkListByPinnedFirst() = runBlockingTest {
+        val link = Link("title", "subtitle", "url", clickedCount = 10)
+        linkDao.insert(link)
+
+        val link2 = Link("title2", "subtitle2", "url2", clickedCount = 5, isPinned = true)
+        linkDao.insert(link2)
+
+        val sortedList = linkDao.getSortedLinkList()
+        assertThat(sortedList.first().title).isEqualTo("title2")
+    }
+
+    @After
+    fun closeDatabase() {
+        database.close()
+    }
+}
