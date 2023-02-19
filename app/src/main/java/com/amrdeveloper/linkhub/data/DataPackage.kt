075499a37bd0ffea72258958d41812cc726ea32f
AmrDeveloper <amr96@programmer.net>
2023-02-19 19:32:39 +0200

    Issue #23: Add serialized name for data package fields

--- app/src/main/java/com/amrdeveloper/linkhub/data/DataPackage.kt
+++ app/src/main/java/com/amrdeveloper/linkhub/data/DataPackage.kt
@@ -1,8 +1,18 @@
 package com.amrdeveloper.linkhub.data
 
+import com.google.gson.annotations.SerializedName
+
 data class DataPackage (
+
+    @SerializedName("folders")
     val folders: List<Folder>,
+
+    @SerializedName("links")
     val links  : List<Link>,
+
+    @SerializedName("showClickCounter")
     val showClickCounter : Boolean?,
+
+    @SerializedName("theme")
     val theme : Theme?
 )
