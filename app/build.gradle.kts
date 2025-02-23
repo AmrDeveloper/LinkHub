c38d7c2c2c34d31a88ac5b9b732d2f3216d0a1ac
AmrDeveloper <amr96@programmer.net>
2025-02-23 20:13:40 +0100

    Update KSP to latest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,9 @@ plugins {
 }
 
 android {
+    buildFeatures.buildConfig = true
     compileSdk = libs.versions.compileSdk.get().toInt()
-
+    namespace = "com.amrdeveloper.linkhub"
     defaultConfig {
         applicationId = "com.amrdeveloper.linkhub"
         minSdk = libs.versions.minSdk.get().toInt()
@@ -36,8 +37,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
