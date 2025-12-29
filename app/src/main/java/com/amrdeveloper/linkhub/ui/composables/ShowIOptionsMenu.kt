669311b1fde447419b0c517661d2392fba33b382
AmrDeveloper <amr96@programmer.net>
2025-12-29 21:16:22 +0100

    Improve the design of ShowOptionsMenu component

--- app/src/main/java/com/amrdeveloper/linkhub/ui/composables/ShowIOptionsMenu.kt
+++ app/src/main/java/com/amrdeveloper/linkhub/ui/composables/ShowIOptionsMenu.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
@@ -47,7 +48,9 @@ fun ShowItemsOptionsDropdownButton(
 
         DropdownMenu(
             expanded = isExpanded,
-            onDismissRequest = { isExpanded = false }
+            onDismissRequest = { isExpanded = false },
+            shape = RoundedCornerShape(16.dp),
+            shadowElevation = 5.dp
         ) {
             options.forEachIndexed { index, option ->
                 DropdownMenuItem(
