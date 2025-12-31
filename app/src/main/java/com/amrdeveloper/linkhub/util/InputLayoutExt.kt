9111f108aba50f0c916ac47ac7f6d2926152365c
AmrDeveloper <amr96@programmer.net>
2026-01-01 00:16:30 +0100

    Remove old XML based extension

--- app/src/main/java/com/amrdeveloper/linkhub/util/InputLayoutExt.kt
+++
@@ -1,7 +0,0 @@
-package com.amrdeveloper.linkhub.util
-
-import com.google.android.material.textfield.TextInputLayout
-
-fun TextInputLayout.showError(errorId: Int) {
-    error = context.getString(errorId)
-}
