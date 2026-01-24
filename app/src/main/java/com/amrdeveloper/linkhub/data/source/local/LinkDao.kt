e7b26bda7d38f5d416afc8ee67e2d5e1766d2de2
AmrDeveloper <amr96@programmer.net>
2026-01-24 09:49:05 +0100

    Issue #17: Extend search functionality to support title, subtile and url

--- app/src/main/java/com/amrdeveloper/linkhub/data/source/local/LinkDao.kt
+++ app/src/main/java/com/amrdeveloper/linkhub/data/source/local/LinkDao.kt
@@ -17,16 +17,21 @@ interface LinkDao : BaseDao<Link> {
     @Query("SELECT * FROM link WHERE folder_id = :id ORDER BY pinned  DESC, click_count DESC")
     suspend fun getSortedLinkListByFolderId(id: Int): List<Link>
 
-    @Query("""
+    @Query(
+        """
         SELECT * FROM link
-        WHERE ((:keyword IS NULL) OR (:keyword = '') OR (title LIKE '%' || :keyword || '%'))
+        WHERE   ((:keyword IS NULL) OR (:keyword = '')
+                OR (title LIKE '%' || :keyword || '%') 
+                OR (subtitle LIKE '%' || :keyword || '%') 
+                OR (url LIKE '%' || :keyword || '%'))
         AND   ((:isPinned IS NULL) OR (pinned = :isPinned))
         AND   ((:isClicked IS NULL) OR (click_count > 0))
         AND   ((:isInsideFolder IS NULL) OR (folder_id != -1))
         AND   ((:folderId IS NULL) OR ((:folderId = -1)) OR (folder_id = :folderId))
         ORDER BY pinned DESC, click_count DESC
         LIMIT :limit
-    """)
+    """
+    )
     fun getSortedLinks(
         keyword: String? = null,
         isPinned: Boolean? = null,
