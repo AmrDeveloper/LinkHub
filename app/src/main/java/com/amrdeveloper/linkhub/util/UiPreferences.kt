7459dbe140405506e8af3e3ab4909dfe825a82ce
AmrDeveloper <amr96@programmer.net>
2026-02-26 21:11:33 +0100

    Improve code in UiPreferences

--- app/src/main/java/com/amrdeveloper/linkhub/util/UiPreferences.kt
+++ app/src/main/java/com/amrdeveloper/linkhub/util/UiPreferences.kt
@@ -25,131 +25,135 @@ private const val PASSWORD_TEXT_KEY = "password_text"
 
 class UiPreferences(private val context: Context) {
 
+    private val preferences by lazy {
+        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+    }
+
     fun setThemeType(theme: Theme) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putString(UI_THEME_KEY, theme.name)
         }
     }
 
     fun setEnableClickCounter(enable: Boolean) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putBoolean(UI_COUNTER_KEY, enable)
         }
     }
 
     fun setEnableQuickActionButton(enable: Boolean) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putBoolean(UI_QUICK_ACTION_BUTTON_KEY, enable)
         }
     }
 
     fun setEnableAutoSave(enable: Boolean) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putBoolean(UI_AUTO_SAVE_KEY, enable)
         }
     }
 
     fun setEnableDefaultFolderEnabled(enable: Boolean) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putBoolean(UI_DEFAULT_FOLDER_KEY, enable)
         }
     }
 
     fun setEnableOpenLinkByClickOption(enable: Boolean) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putBoolean(LINK_DEFAULT_ACTION_KEY, enable)
         }
     }
 
     fun setMinimalModeEnabled(enable: Boolean) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putBoolean(UI_MINIMAL_MODE_KEY, enable)
         }
     }
 
     fun setDefaultFolderId(folderId: Int) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putInt(DEFAULT_FOLDER_NAME, folderId)
         }
     }
 
     fun deleteDefaultFolder() {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putInt(DEFAULT_FOLDER_NAME, -1)
         }
     }
 
     fun getThemeType(): Theme {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         val themeName = preferences.getString(UI_THEME_KEY, Theme.WHITE.name)
         return Theme.valueOf(themeName.toString())
     }
 
     fun setFontFamilyName(name: String) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putString(UI_FONT_FAMILY_KEY, name)
         }
     }
 
     fun setEnablePassword(enable: Boolean) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putBoolean(PASSWORD_ENABLE_KEY, enable)
         }
     }
 
     fun setPasswordText(password: String) {
-        context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE).edit {
+        preferences.edit {
             putString(PASSWORD_TEXT_KEY, password)
         }
     }
 
     fun isClickCounterEnabled(): Boolean {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getBoolean(UI_COUNTER_KEY, true)
     }
 
     fun isQuickActionButtonEnabled(): Boolean {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getBoolean(UI_QUICK_ACTION_BUTTON_KEY, false)
     }
 
     fun isAutoSavingEnabled(): Boolean {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getBoolean(UI_AUTO_SAVE_KEY, true)
     }
 
     fun isDefaultFolderEnabled(): Boolean {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getBoolean(UI_DEFAULT_FOLDER_KEY, false)
     }
 
     fun isMinimalModeEnabled(): Boolean {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getBoolean(UI_MINIMAL_MODE_KEY, false)
     }
 
     fun isOpenLinkByClickOptionEnabled(): Boolean {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getBoolean(LINK_DEFAULT_ACTION_KEY, false)
     }
 
     fun getDefaultFolderId(): Int {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getInt(DEFAULT_FOLDER_NAME, -1)
     }
 
     fun getFontFamilyName(): String {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getString(UI_FONT_FAMILY_KEY, "Default") ?: "Default"
     }
 
     fun isPasswordEnabled(): Boolean {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getBoolean(PASSWORD_ENABLE_KEY, false)
     }
 
     fun getPasswordText(): String {
-        val preferences = context.getSharedPreferences(UI_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        val preferences = preferences
         return preferences.getString(PASSWORD_TEXT_KEY, "") ?: ""
     }
 }
